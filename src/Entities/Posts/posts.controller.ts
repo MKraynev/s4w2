ce0aed485c6b6214a97bb449041bab1aa73ef9ba
Matthew Kraynev <becaury@gmail.com>
2023-10-30 00:37:19 +0100

    Промежуточный коммит

--- src/Entities/Posts/posts.controller.ts
+++ src/Entities/Posts/posts.controller.ts
@@ -1,22 +1,136 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Post, Put, Query, UseGuards } from "@nestjs/common";
+import { PostService } from "./posts.service";
+import { ServiceExecutionResultStatus } from "../../Common/Services/Types/ServiceExecutionStatus";
+import { CreatePostDto } from "./PostsRepo/Dtos/CreatePostDto";
+import { QueryPaginator } from "../../Common/Routes/QueryParams/PaginatorQueryParams";
+import { PostDto } from "./PostsRepo/Schema/post.schema";
+import { InputPaginator, OutputPaginator } from "../../Common/Paginator/Paginator";
+import { LikeService } from "../Likes/likes.service";
 import { CreateCommentDto } from "../Comments/CommentsRepo/Dto/CreateCommentDto";
 
-@Controller('posts')
+@Controller("posts")
 export class PostController {
+    constructor(private postService: PostService, private likeService: LikeService) { }
 
-    //put -> /hometask_14/api/posts/{postId}/like-status
+    @Get()
+    async GetPosts(
+        @Query('sortBy') sortBy: keyof (PostDto) = "createdAt",
+        @Query('sortDirection') sortDirecrion: "desc" | "asc" = "desc",
+        @QueryPaginator() paginator: InputPaginator
 
-    //get -< /hometask_14/api/posts/{postId}/like-status
+    ) {
+        let findPost = await this.postService.Take(sortBy, sortDirecrion, undefined, undefined, paginator.skipElements, paginator.pageSize);
 
-    //post => /hometask_14/api/posts/{postId}/comments
-    @Post(':id/comments')
-    async SaveComment(@Body() commentData: CreateCommentDto) {
+        switch (findPost.executionStatus) {
+            case ServiceExecutionResultStatus.Success:
+                let count = findPost.executionResultObject.count;
+                let decoratedPosts = await Promise.all(findPost.executionResultObject.items.map(async (post) => {
 
+                    let { updatedAt, ...rest } = post;
+                    let decoratedPost = await this.likeService.DecorateWithExtendedInfo(rest.id, rest)
+                    return decoratedPost;
+                }));
+                let pagedPosts = new OutputPaginator(count, decoratedPosts, paginator)
+                return pagedPosts;
+                break;
+
+            default:
+                return;
+        }
     }
-    //get -< /hometask_14/api/posts
-    //post -> /hometask_14/api/posts
-    //get -> /hometask_14/api/posts/{id}
-    //put -> /hometask_14/api/posts/{id}
-    //delete -> /hometask_14/api/posts/{id}
 
+    //get -> /hometask_13/api/posts/{id}
+    @Get(":id")
+    async GetPostById(@Param('id') id: string) {
+        let findPost = await this.postService.TakeById(id);
+
+        switch (findPost.executionStatus) {
+            case ServiceExecutionResultStatus.Success:
+                let { updatedAt, ...returnPost } = findPost.executionResultObject;;
+                let decoratedPost = await this.likeService.DecorateWithExtendedInfo(returnPost.id, returnPost);
+                return decoratedPost;
+                break;
+
+            default:
+            case ServiceExecutionResultStatus.NotFound:
+                throw new NotFoundException();
+                break;
+        }
+    }
+
+    //get -> hometask_13/api/posts/{postId}/comments
+    @Get(":id/comments")
+    async GetPostComments() {
+        return ["some comment 1", "some comment 2"];
+    }
+
+    @Post(':id/comments')
+    async SaveComment(
+        @Param('id') id: string,
+        @Body() commentData: CreateCommentDto) {
+        return id;
+    }
+
+    //post -> /hometask_13/api/posts
+    @Post()
+    async SavePost(@Body() post: CreatePostDto) {
+        let savePost = await this.postService.CreateByBlogId(post.blogId, post);
+
+        switch (savePost.executionStatus) {
+            case ServiceExecutionResultStatus.Success:
+                //TODO Возвращаемая сущность содержит две инфы от постов и лайков
+                //Задавать логику лайков в посты нет желания
+                //Делать отдельный Join repo?
+                let { updatedAt, ...returnPost } = savePost.executionResultObject;
+                let decoratedPost = await this.likeService.DecorateWithExtendedInfo(returnPost.id, returnPost);
+                return decoratedPost;
+                break;
+
+            default:
+                throw new BadRequestException();
+                break;
+        }
+    }
+
+    //put -> /hometask_13/api/posts/{id}
+    @Put(":id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async UpdatePost(
+        @Param("id") id: string,
+        @Body() postData: CreatePostDto
+    ) {
+        let updatePost = await this.postService.Update(id, postData);
+
+        switch (updatePost.executionStatus) {
+            case ServiceExecutionResultStatus.Success:
+                let { updatedAt, ...returnPost } = updatePost.executionResultObject;
+                let decoratedPost = await this.likeService.DecorateWithExtendedInfo(returnPost.id, returnPost);
+
+                return decoratedPost;
+                break;
+
+            default:
+            case ServiceExecutionResultStatus.NotFound:
+                throw new NotFoundException();
+                break;
+        }
+    }
+
+    //delete -> /hometask_13/api/posts/{id}
+    @Delete(":id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async DeletePost(@Param('id') id: string) {
+        let deletePost = await this.postService.Delete(id);
+
+        switch (deletePost.executionStatus) {
+            case ServiceExecutionResultStatus.Success:
+                return;
+                break;
+
+            default:
+            case ServiceExecutionResultStatus.NotFound:
+                throw new NotFoundException();
+                break;
+        }
+    }
 }
--- src/Entities/Posts/posts.module.ts
+++ src/Entities/Posts/posts.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './posts.service';
-import { PostController } from './Controller/posts.controller';
+import { PostController } from './posts.controller';
 import { PostsRepoModule } from './PostsRepo/postsRepo.module';
 import { BlogsRepoModule } from '../Blogs/BlogsRepo/blogsRepo.module';
 import { LikesModule } from '../Likes/likes.module';
