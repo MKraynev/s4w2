bf398221df1d4c4b5affd3635ba1e261ce5f6747
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2023-11-07 23:38:43 +0100

    Добавил проверку на валидацию blogId == mongoIdObject

--- src/Entities/Posts/Repo/Dtos/posts.createPostDto.ts
+++ src/Entities/Posts/Repo/Dtos/posts.createPostDto.ts
@@ -1,7 +1,8 @@
-import { IsString, MaxLength } from "class-validator";
+import { IS_LENGTH, IsString, IsUUID, MaxLength } from "class-validator";
 import { Blogs_CreatePostDto } from "../../../Blogs/Entities/blogs.createPostDto";
+import { IsMongoIdObject } from "../../../../Common/Validators/validator.IsMongoIdObject";
 
 export class Post_CreatePostDto extends Blogs_CreatePostDto{
-    @IsString()
+    @IsMongoIdObject()
     public blogId: string;
 }
